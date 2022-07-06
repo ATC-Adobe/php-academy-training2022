dd327c6455ae15f2ee3e45cc121c08697b445815
paszekdaniel <daniel.paszek@accenture.com>
2022-07-06 16:47:54 +0200

    add navbar

--- reserveForm.php
+++ reserveForm.php
@@ -8,6 +8,23 @@
 
 </head>
 <body class="background">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item ">
+                <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="/reservationList.php">Current reservations</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
     <form class="container mt-4" method="post" action="reservationList.php" >
         <div class="row my-3">
             <h1 class="w-100 text-center">
